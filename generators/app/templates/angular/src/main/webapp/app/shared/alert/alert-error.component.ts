3039716e5e2caab903627f52f09e6b7f0532dccf
ChihoSin <chihosin@icloud.com>
2018-04-29 14:52:22 +0800

    Added func to remove bootstrap ui

--- generators/app/templates/angular/src/main/webapp/app/shared/alert/alert-error.component.ts
+++ generators/app/templates/angular/src/main/webapp/app/shared/alert/alert-error.component.ts
@@ -33,60 +33,62 @@ export class <%=jhiPrefixCapitalized%>AlertErrorComponent implements OnDestroy {
         /* tslint:enable */
         this.alerts = [];
 
-        this.cleanHttpErrorListener = eventManager.subscribe('<%=angularAppName%>.httpError', (response) => {
-            let i;
-            const httpErrorResponse = response.content;
-            switch (httpErrorResponse.status) {
-                // connection refused, server not reachable
-                case 0:
-                    this.addErrorAlert('Server not reachable', 'error.server.not.reachable');
-                    break;
+        this.cleanHttpErrorListener = eventManager.subscribe('<%=angularAppName%>.httpError',
+            (response) => {
+                let i;
+                const httpErrorResponse = response.content;
+                switch (httpErrorResponse.status) {
+                    // connection refused, server not reachable
+                    case 0:
+                        this.addErrorAlert('Server not reachable', 'error.server.not.reachable');
+                        break;
 
-                case 400:
-                    const arr = httpErrorResponse.headers.keys();
-                    let errorHeader = null;
-                    let entityKey = null;
-                    arr.forEach((entry) => {
-                        if (entry.endsWith('app-error')) {
-                            errorHeader = httpErrorResponse.headers.get(entry);
-                        } else if (entry.endsWith('app-params')) {
-                            entityKey = httpErrorResponse.headers.get(entry);
+                    case 400:
+                        const arr = httpErrorResponse.headers.keys();
+                        let errorHeader = null;
+                        let entityKey = null;
+                        arr.forEach((entry) => {
+                            if (entry.endsWith('app-error')) {
+                                errorHeader = httpErrorResponse.headers.get(entry);
+                            } else if (entry.endsWith('app-params')) {
+                                entityKey = httpErrorResponse.headers.get(entry);
+                            }
+                        });
+                        if (errorHeader) {
+                            const entityName = <% if (enableTranslation) { %>translateService.instant('global.menu.entities.' + entityKey)<% }else{ %>entityKey<% } %>;
+                            this.addErrorAlert(errorHeader, errorHeader, { entityName });
+                        } else if (httpErrorResponse.error !== '' && httpErrorResponse.error.fieldErrors) {
+                            const fieldErrors = httpErrorResponse.error.fieldErrors;
+                            for (i = 0; i < fieldErrors.length; i++) {
+                                const fieldError = fieldErrors[i];
+                                // convert 'something[14].other[4].id' to 'something[].other[].id' so translations can be written to it
+                                const convertedField = fieldError.field.replace(/\[\d*\]/g, '[]');
+                                const fieldName = <% if (enableTranslation) { %>translateService.instant('<%=angularAppName%>.' +
+                                    fieldError.objectName + '.' + convertedField)<% } else { %>convertedField.charAt(0).toUpperCase() +
+                                convertedField.slice(1)<% } %>;
+                                this.addErrorAlert(
+                                    'Error on field "' + fieldName + '"', 'error.' + fieldError.message, { fieldName });
+                            }
+                        } else if (httpErrorResponse.error !== '' && httpErrorResponse.error.message) {
+                            this.addErrorAlert(httpErrorResponse.error.message, httpErrorResponse.error.message, httpErrorResponse.error.params);
+                        } else {
+                            this.addErrorAlert(httpErrorResponse.error);
                         }
-                    });
-                    if (errorHeader) {
-                        const entityName = <% if (enableTranslation) { %>translateService.instant('global.menu.entities.' + entityKey)<% }else{ %>entityKey<% } %>;
-                        this.addErrorAlert(errorHeader, errorHeader, { entityName });
-                    } else if (httpErrorResponse.error !== '' && httpErrorResponse.error.fieldErrors) {
-                        const fieldErrors = httpErrorResponse.error.fieldErrors;
-                        for (i = 0; i < fieldErrors.length; i++) {
-                            const fieldError = fieldErrors[i];
-                            // convert 'something[14].other[4].id' to 'something[].other[].id' so translations can be written to it
-                            const convertedField = fieldError.field.replace(/\[\d*\]/g, '[]');
-                            const fieldName = <% if (enableTranslation) { %>translateService.instant('<%=angularAppName%>.' +
-                                fieldError.objectName + '.' + convertedField)<% } else { %>convertedField.charAt(0).toUpperCase() +
-                            convertedField.slice(1)<% } %>;
-                            this.addErrorAlert(
-                                'Error on field "' + fieldName + '"', 'error.' + fieldError.message, { fieldName });
+                        break;
+
+                    case 404:
+                        this.addErrorAlert('Not found', 'error.url.not.found');
+                        break;
+
+                    default:
+                        if (httpErrorResponse.error !== '' && httpErrorResponse.error.message) {
+                            this.addErrorAlert(httpErrorResponse.error.message);
+                        } else {
+                            this.addErrorAlert(httpErrorResponse.error);
                         }
-                    } else if (httpErrorResponse.error !== '' && httpErrorResponse.error.message) {
-                        this.addErrorAlert(httpErrorResponse.error.message, httpErrorResponse.error.message, httpErrorResponse.error.params);
-                    } else {
-                        this.addErrorAlert(httpErrorResponse.error);
-                    }
-                    break;
-
-                case 404:
-                    this.addErrorAlert('Not found', 'error.url.not.found');
-                    break;
-
-                default:
-                    if (httpErrorResponse.error !== '' && httpErrorResponse.error.message) {
-                        this.addErrorAlert(httpErrorResponse.error.message);
-                    } else {
-                        this.addErrorAlert(httpErrorResponse.error);
-                    }
+                }
             }
-        });
+        );
     }
 
     ngOnDestroy() {
